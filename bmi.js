21ca974ee745596a30a533410bcdb9289e2ea509
ahmedcheraif31 <ahmedcheraif31@gmail.com>
2023-03-06 17:33:19 +0100

    t

--- bmi.js
+++ bmi.js
@@ -1,25 +1,30 @@
 
 // function that converte the weight on Pounds
+var x=document.getElementById("outputPounds").value;
+console.log(x)
 function weightConverter(valNum) {
     document.getElementById("outputPounds").innerHTML=valNum*2.2046;
-console.log("value : "+valNum)
 
   }
-// function that converte the weight on Kilograms
+ 
 
-  function convertPoundsToKilograms(pounds) {
-document.getElementById("outputKilograms").innerHTML= pounds * 0.453592;
-  }
+// // function that converte the weight on Kilograms
 
-// function that converte the length on Metres 
+//   function convertPoundsToKilograms(pounds) {
+// document.getElementById("outputKilograms").innerHTML= pounds * 0.453592;
+//   }
 
-  function LengthConverter(valNum) {
-    document.getElementById("outputMeters").innerHTML=valNum/3.2808;
-  }
+// // function that converte the length on Metres 
+
+//   function LengthConverter(valNum) {
+//     document.getElementById("outputMeters").innerHTML=valNum/3.2808;
+//   }
 
 // function that converte the length on feets 
 
   function LengthConverter1(valNum) {
+
     document.getElementById("outputFeet").innerHTML=valNum*3.2808;
+
   }
   
